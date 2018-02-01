9fb2cf7f00ba367ecdd895d0b1374e6129e1b310
Maksym <gopkomax@gmail.com>
2018-02-01 13:57:34 +0200

    Created a project from the archetype.

---
+++ src/main/java/org/maxstore/Application.java
@@ -0,0 +1,3 @@
+package org.maxstore;
+
+public interface Application {}
---
+++ src/test/java/org/maxstore/signup/SignupControllerTest.java
@@ -0,0 +1,24 @@
+package org.maxstore.signup;
+
+import org.junit.Test;
+import org.maxstore.config.WebSecurityConfigurationAware;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class SignupControllerTest extends WebSecurityConfigurationAware {
+    @Test
+    public void displaysSignupForm() throws Exception {
+        mockMvc.perform(get("/signup"))
+                .andExpect(model().attributeExists("signupForm"))
+                .andExpect(view().name("signup/signup"))
+                .andExpect(content().string(
+                        allOf(
+                                containsString("<title>Signup</title>"),
+                                containsString("<legend>Please Sign Up</legend>")
+                        ))
+                );
+    }
+}
